7a4e6c7026213c74206044ac66d296ead79ceee9
anooj1483 <anooj1483@gmail.com>
2015-02-05 12:55:23 +0530

    Network Class added

--- src/com/team/cafebeside/configs/Configuration.java
+++ src/com/team/cafebeside/configs/Configuration.java
@@ -9,4 +9,5 @@ public class Configuration {
 
 	public static String CAFE_WIFI_SSID		=	"dian_alley";//"D-Link_DIR-600M"; //"MBLAZE-DF800i-125A";
 	public static boolean IS_APP_RUNNING	=	false;
+	public static String USERNAME		=	null;
 }
---
+++ src/com/team/cafebeside/networkEngine/HttpRequestWorker.java
@@ -0,0 +1,78 @@
+package com.team.cafebeside.networkEngine;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import com.team.cafebeside.configs.Configuration;
+
+import android.util.Log;
+
+/**
+ * Created by anoojkrishnang on 14/12/14.
+ * 
+ */
+public class HttpRequestWorker {
+	
+	
+
+	/*
+	 * Method: GetRequest
+	 * @param: url:String,isHeaderRequired:boolean
+	 * @Desc : url				: URL to connect the server, 
+	 *         isHeaderRequired	: Set username as Header for every request after Login.
+	 */
+	public String GetRequest(String url, boolean isHeaderRequired) {
+
+		try {
+			HttpClient mClient = new DefaultHttpClient();
+			HttpGet mHttpGet = new HttpGet(url);
+			if (isHeaderRequired) {
+				mHttpGet.setHeader("username", Configuration.USERNAME);
+			}
+			ResponseHandler<String> mResponseHandler = new BasicResponseHandler();
+			String mStatus = mClient.execute(mHttpGet, mResponseHandler);
+			Log.e("RESPONSE", "" + mStatus);
+			return mStatus;
+		} catch (Exception ex) {
+			return "Failed " + ex;
+		}
+	}
+
+	/*
+	 * Method: POSTRequest
+	 * @param: url:String, content:String, isHeaderRequired:boolean
+	 * @Desc : url				: URL to connect the server,
+	 *         content			: JSON Content to send, 
+	 *         isHeaderRequired	: Set username as Header for every request after Login.
+	 */
+	public String PostRequest(String url, String content,
+			boolean isHeaderRequired) {
+		try {
+			String mStatus = null;
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			HttpPost mHttpPost = new HttpPost(url);
+			StringEntity se = new StringEntity(content);
+			se.setContentType("application/json");
+			mHttpPost.setEntity(se);
+			if (isHeaderRequired) {
+				mHttpPost.setHeader("username", Configuration.USERNAME);
+			}
+
+			HttpResponse httpresponse = httpclient.execute(mHttpPost);
+			mStatus = EntityUtils.toString(httpresponse.getEntity());
+
+			Log.e("RESPONSE", "" + mStatus);
+
+			return mStatus;
+		} catch (Exception ex) {
+			return "Failed " + ex;
+		}
+	}
+}
