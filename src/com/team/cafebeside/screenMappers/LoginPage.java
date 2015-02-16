f3151f45cd98d7e0ee3a624ac7285eb090b3e6f9
ferose_ali <feroseali404@gmail.com>
2015-02-16 22:48:44 +0530

    Change Login code a little bit

--- src/com/team/cafebeside/screenMappers/LoginPage.java
+++ src/com/team/cafebeside/screenMappers/LoginPage.java
@@ -1,16 +1,19 @@
 package com.team.cafebeside.screenMappers;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import com.team.cafebeside.R;
-import com.team.cafebeside.configs.Configuration;
-import com.team.cafebeside.networkEngine.HttpRequestWorker;
+import com.team.cafebeside.networkEngine.PostMan;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,11 +30,19 @@ public class LoginPage extends Activity{
 	public static String upassword = null;
 	private Button mSignIn;			// Login button
 	private Button mSignUp;			// Register Button
+	private final String DB_NAME = "ferose_cafebeside.db";
+	private SQLiteDatabase db = null;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		db = openOrCreateDatabase(DB_NAME, MODE_PRIVATE, null);
+		db.execSQL("CREATE TABLE IF NOT EXISTS LOGIN(NAME VARCHAR(50),EMAIL VARCHAR(30),PHONE VARCHAR(15),PASSWORD VARCHAR(15),PRIMARY KEY(EMAIL))");
+		db.execSQL("delete from LOGIN");
 		
 		username = (EditText) findViewById(R.id.etUserName);
 		userpass = (EditText) findViewById(R.id.etPass);
@@ -44,19 +55,8 @@ public class LoginPage extends Activity{
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				usermail = username.getText().toString();
-				upassword = userpass.getText().toString();
-				if(!usermail.equals("") && !upassword.equals("")){
-
-					new AttemptLogin().execute();
-				}
-				else{
-					Toast.makeText(getApplicationContext(), "Please enter your login details.",Toast.LENGTH_LONG).show();
-				}
-
-					//Log.e("","");
-					new AttemptLogin().execute();
-				}
+				new AttemptLogin().execute();
+			}
 			
 		});
 		
@@ -117,17 +117,12 @@ public class LoginPage extends Activity{
 	/*********async task *************/
 	
 	class AttemptLogin extends AsyncTask<String, String, String> {
-
-
+		final JSONObject mJson = new JSONObject();
         String response;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-
-            //Toast.makeText(getBaseContext(), "REACHED", Toast.LENGTH_LONG);
-
-
             progress = new ProgressDialog(LoginPage.this);
             progress.setTitle("Processing");
             progress.setMessage("Wait while loading...");
@@ -139,22 +134,101 @@ public class LoginPage extends Activity{
         @Override
         protected String doInBackground(String... args) {
 
-        	try{
-            	JSONObject mObject = new JSONObject();
-            	mObject.put("username", username);
-            	mObject.put("password",userpass);
-            	
-            	HttpRequestWorker mWorker = new HttpRequestWorker();
+			runOnUiThread(new Runnable() {
 
-            	 response = mWorker.PostRequest("http://192.168.0.2/cafebeside/login.php", mObject.toString(), false);
-   
-            	 return response;
-            	
-                }catch(Exception e){
-                	return null;
-                }
+				@Override
+				public void run() {
+					usermail = username.getText().toString();
+					upassword = userpass.getText().toString();
 
-            
+					if (usermail.matches("") || upassword.matches("")) {
+						Toast.makeText(getBaseContext(), "Please fill the fileds first!",Toast.LENGTH_LONG).show();
+		        		showAlert("Alert","Invalid UserName or Password");
+
+					} else {
+
+						try {
+			        		mJson.put("username", usermail);
+			        		mJson.put("password",upassword);
+
+							final String json_string = mJson.toString();
+
+							//PostMan mSender = new PostMan();
+
+							Thread thread = new Thread(new Runnable() {
+								@Override
+								public void run() {
+									try {
+										// Your code goes here
+										PostMan mSender = new PostMan();
+										response = mSender.post(json_string,"http://feroseali.in/cafebeside/login.php");
+
+										if (!response.contains("Failed")) {
+
+											try {
+
+												JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+												String mName = object
+														.getString("name");
+												String mEmail = object
+														.getString("email");
+												String mPhn = object
+														.getString("phone");
+												String mPassword = object
+														.getString("password");
+
+												mName = DatabaseUtils
+														.sqlEscapeString(mName);
+												mEmail = DatabaseUtils
+														.sqlEscapeString(mEmail);
+												mPhn = DatabaseUtils
+														.sqlEscapeString(mPhn);
+												mPassword = DatabaseUtils
+														.sqlEscapeString(mPassword);
+
+												db.execSQL("INSERT INTO LOGIN VALUES("
+														+ mName
+														+ ","
+														+ mEmail
+														+ ","
+														+ mPhn
+														+ ","
+														+ mPassword + ")");
+												Intent intent1 = new Intent(getApplicationContext(),HomeActivity.class);
+												startActivity(intent1);
+												finish();
+
+											} catch (Exception e) {
+												Toast.makeText(
+														getBaseContext(),
+														"Check Internet Availability",
+														Toast.LENGTH_SHORT)
+														.show();
+											}
+
+										} else {
+											Toast.makeText(getBaseContext(),
+													"Login Failed",
+													Toast.LENGTH_LONG).show();
+											username.setText("");
+											userpass.setText("");
+										}
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+								}
+							});
+
+							thread.start();
+
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					}
+
+				}
+			});
+			return null;  
 
         }
 
@@ -165,17 +239,8 @@ public class LoginPage extends Activity{
         protected void onPostExecute(String fserve) {
         	Log.e("response",fserve);
             progress.dismiss();
-
-            if(response.equals("Success")){
-            	Configuration.USERNAME	=	usermail;
-        		Intent mBeside = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(mBeside);
-        	}else{
-
-        		Intent mBeside = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(mBeside);
-        		showAlert("Alert","Invalid UserName or Password");
-        	}
+    
+            
         }
 
     }
